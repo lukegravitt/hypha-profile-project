85f2c21a3a8b747e02033df29c81934533270e14
lukegravitt <lukegravitt@gmail.com>
2021-03-04 11:07:43 -0800

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -9,16 +9,14 @@ quasar build
 # navigate into the build output directory
 cd dist/spa
 
-# if you are deploying to a custom domain
-
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lukegravitt/hypha-profile-project.git main:gh-pages
+git push -f git@github.com:lukegravitt/hypha-profile-project.git master:gh-pages
 
 cd -
